71b00dae7e7dd90136ed8c1fbe776c15ed5c0b8b
Aleksey Paslenov <91953433+Wolroys@users.noreply.github.com>
2024-10-06 08:51:16 +0300

    add-tests (#12)
    * Добавлены новые тесты
    
    * Добавлены новые тесты для проверки EventServiceImpl
    
    Signed-off-by: Aleksey <alekseypaslenov@gmail.com>
    
    * Тесты отредактированы
    
    Signed-off-by: Aleksey <alekseypaslenov@gmail.com>
    
    ---------
    
    Signed-off-by: Aleksey <alekseypaslenov@gmail.com>

--- src/main/java/com/wolroys/wellbeing/domain/event/EventRepository.java
+++ src/main/java/com/wolroys/wellbeing/domain/event/EventRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,5 +13,5 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     @Query(value = "select * from Event e " +
             "where (:title is null or lower(e.title) like lower(concat('%', :title, '%')))", nativeQuery = true)
-    Page<Event> findAll(Pageable pageable, String title);
+    Page<Event> findAll(Pageable pageable, @Param("title") String title);
 }
---
+++ src/test/java/com/wolroys/wellbeing/event/EventServiceImplTest.java
@@ -0,0 +1,112 @@
+package com.wolroys.wellbeing.event;
+
+import com.wolroys.wellbeing.domain.event.EventRepository;
+import com.wolroys.wellbeing.domain.event.entity.Event;
+import com.wolroys.wellbeing.domain.event.entity.EventDto;
+import com.wolroys.wellbeing.domain.event.entity.EventRequestDto;
+import com.wolroys.wellbeing.domain.event.entity.Status;
+import com.wolroys.wellbeing.domain.event.service.EventServiceImpl;
+import com.wolroys.wellbeing.domain.user.entity.User;
+import com.wolroys.wellbeing.util.PostgreSQLContainerInitializer;
+import com.wolroys.wellbeing.util.exception.EventNotFoundException;
+import com.wolroys.wellbeing.util.exception.IncorrectDateException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SpringBootTest
+public class EventServiceImplTest extends PostgreSQLContainerInitializer {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private EventServiceImpl eventService;
+
+    @Test
+    void checkThatAllElementsWillReturn() {
+        Event event1 = new Event();
+        event1.setTitle("Event 1");
+        Event event2 = new Event();
+        event2.setTitle("Event 2");
+
+        eventRepository.saveAllAndFlush(List.of(event1, event2));
+
+        List<EventDto> founded = eventService.findAll(Pageable.unpaged(), null);
+
+        Assertions.assertFalse(founded.isEmpty());
+    }
+
+    @Test
+    void repositoryShouldReturnOnlyOneEntityByTitle() {
+        Event event1 = new Event();
+        event1.setTitle("Event 1");
+        Event event2 = new Event();
+        event2.setTitle("Event 2");
+        eventRepository.saveAll(List.of(event1, event2));
+
+        Page<Event> events = eventRepository.findAll(Pageable.unpaged(), "1");
+
+        Assertions.assertEquals(1, events.getTotalElements());
+    }
+
+    @Test
+    void testShouldThrowException() {
+        Assertions.assertThrows(EventNotFoundException.class, () -> eventService.findById(1001L),
+                "This event doesn't exist");
+    }
+
+    @Test
+    void testShouldReturnSavedEvent() {
+        Event event1 = new Event();
+        event1.setTitle("Saved entity");
+
+        Event event = eventRepository.saveAndFlush(event1);
+
+        eventService.findById(event.getId());
+        Assertions.assertNotNull(event);
+        Assertions.assertEquals(event1.getTitle(), event.getTitle());
+    }
+
+    @Test
+    void eventShouldBeSaved() {
+        EventRequestDto eventRequestDto = new EventRequestDto()
+                .setTitle("Title")
+                .setDescription("Description")
+                .setUrl("URL")
+                .setStatus(Status.PLANNED)
+                .setStartDate(LocalDateTime.now())
+                .setEndDate(LocalDateTime.now().plusHours(1));
+
+        User user = new User();
+        user.setEmail("test@mail.ru");
+        user.setId(1L);
+
+        EventDto eventDto = eventService.create(eventRequestDto);
+
+        Assertions.assertNotNull(eventDto);
+        Assertions.assertEquals(eventRequestDto.getTitle(), eventDto.getTitle());
+        Assertions.assertEquals(eventRequestDto.getDescription(), eventDto.getDescription());
+        Assertions.assertEquals(eventRequestDto.getUrl(), eventDto.getUrl());
+        Assertions.assertEquals(eventRequestDto.getStatus(), eventDto.getStatus());
+        Assertions.assertEquals(eventRequestDto.getStartDate(), eventDto.getStartDate());
+        Assertions.assertEquals(eventRequestDto.getEndDate(), eventDto.getEndDate());
+    }
+
+    @Test
+    void testShouldReturnExceptionThatStartDateAfterEndDate() {
+        EventRequestDto eventRequestDto = new EventRequestDto();
+        eventRequestDto.setTitle("Title");
+        eventRequestDto.setStartDate(LocalDateTime.now().plusHours(1));
+        eventRequestDto.setEndDate(LocalDateTime.now());
+
+        Assertions.assertThrows(IncorrectDateException.class, () -> eventService.create(eventRequestDto),
+                "Start date cannot be after end date");
+    }
+}
