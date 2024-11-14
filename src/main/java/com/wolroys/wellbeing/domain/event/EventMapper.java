54ec8e9be63c7e56db116e0c383093ef2e899c2f
Aleksey <alekseypaslenov@gmail.com>
2024-11-14 22:07:40 +0300

    Добавлено новое DTO

--- src/main/java/com/wolroys/wellbeing/domain/event/EventMapper.java
+++ src/main/java/com/wolroys/wellbeing/domain/event/EventMapper.java
@@ -3,16 +3,16 @@ package com.wolroys.wellbeing.domain.event;
 import com.wolroys.wellbeing.domain.event.entity.Event;
 import com.wolroys.wellbeing.domain.event.entity.EventDto;
 import com.wolroys.wellbeing.domain.event.entity.EventRequestDto;
+import com.wolroys.wellbeing.domain.event.entity.UnauthorizedEventDto;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface EventMapper {
 
-    EventMapper INSTANCE = Mappers.getMapper(EventMapper.class);
-
     Event toEntity(EventRequestDto eventRequestDto);
 
     EventDto toDto(Event event);
+
+    UnauthorizedEventDto toUnauthorizedEventDto(Event event);
 }
---
+++ src/main/java/com/wolroys/wellbeing/domain/event/entity/UnauthorizedEventDto.java
@@ -0,0 +1,19 @@
+package com.wolroys.wellbeing.domain.event.entity;
+
+import com.wolroys.wellbeing.domain.user.entity.UserDto;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class UnauthorizedEventDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private UserDto speaker;
+}
